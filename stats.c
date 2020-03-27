19e0343ac1f864e553287b7090e203e0c74e8a50
shubham <6shubham6yadav@gmail.com>
2020-03-27 18:41:42 +0530

    adding final code and documentation for c1m1 assessment

--- stats.c
+++ stats.c
@@ -9,10 +9,9 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <stats.c> 
+ * @brief <assignment 1 >
  *
- * <Add Extended Description Here>
  *
  * @author <shubham yadav>
  * @date <26/03/2020>
@@ -23,27 +22,111 @@
 
 #include <stdio.h>
 #include "stats.h"
-
-/* Size of the Data Set */
-#define SIZE (40)
-void print_statistics(int min,int max,int mean,int median){
+/**************************FUNCTIONS**********************************************/
+//PRINTS STATISTICS
+void print_statistics(int min,int max,float mean,float median){
+printf("\nminimum is: %d ",min);
+printf("\nmaximum is: %d ",max);
+printf("\nmean is: %.2f ",mean);
+printf("\nmedian is: %.2f\n ",median);
 }
-void print_array(int elm){
+//PRINTS GIVEN ARRAY
+void print_array(int arr[],int n){
+int i;
+printf("given elements: ");
+for(i=0;i<n;i++)
+printf("%d  ",arr[i]);
 }
-void find_min(int min){
+//FINDS MINIMUM AND RETURNS TO MAIN FUNCTION
+int find_min(int arr[],int n){
+int min,i;
+min=arr[0];
+for(i=1;i<n;i++)
+{
+if(arr[i]<min)
+{min=arr[i];
 }
-void find_max(int max){
 }
-void find_median(int median){
+return (min);
 }
-void find_mean(int mean){
+//FINDS MAXIMUM AND RETURNS TO MAIN FUNCTION
+int find_max(int arr[],int n){
+int max,i;
+max=arr[0];
+for(i=1;i<n;i++)
+{
+if(arr[i]>max)
+{max=arr[i];
 }
-void sort_array(int elm,int num){
 }
-
+return (max);
+}
+//FINDS MEDIAN AND RETURNS TO MAIN FUNCTION
+float find_median(int arr[],int n){
+float i;
+int j;
+if(n%2==0)
+{
+j=n/2;
+i=(arr[j]+arr[j-1])/2.0;
+}
+else{
+j=(n/2)+1;
+i=arr[n/2];
+}
+return (i);
+}
+//FINDS MEAN AND RETURNS TO MAIN FUNCTION
+float find_mean(int arr[],int n){
+int i;
+float sum=0.0,mean;
+for(i=0;i<n;i++)
+sum=sum+arr[i];
+mean=(sum/n)/1.0;
+return (mean);
+}
+//SORTS THE GIVEN ARRAY FROM LARGEST TO SMALLEST
+void sort_array(int arr[],int n){
+int i,j,temp;
+for(i=0;i<n-1;i++)
+{for(j=0;j<n-1-i;j++)
+{if(arr[j]<arr[j+1])
+{temp=arr[j];
+arr[j]=arr[j+1];
+arr[j+1]=temp;
+}
+}
+}
+}
+/***************MAIN FUNCTION*********************************/
 void main() {
 
- 
+int arr[50],n,i,min,max;
+float median,mean;
+
+printf("enter number of elements\n");
+scanf("%d",&n);
+
+printf("enter elements\n");
+for(i=0;i<n;i++)
+scanf("%d",&arr[i]);
+
+print_array(arr,n);
+printf("\n");
+
+min=find_min(arr,n);
+max=find_max(arr,n);
+
+sort_array(arr,n);
+printf("sorted array is: ");
+for(i=0;i<n;i++)
+printf("%d  ",arr[i]);
+
+median=find_median(arr,n);
+mean=find_mean(arr,n);
+
+print_statistics(min,max,mean,median);
+
 }
 
-/* Add other Implementation File Code Here */
+
--- stats.h
+++ stats.h
@@ -9,32 +9,76 @@
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file <stats.h> 
+ * @brief <file for assignment 1>
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author <Shubham Yadav>
+ * @date <27th march 2020>
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
+void print_statistics(int min,int max,float mean,float median)
 
-/**
- * @brief <Add Brief Description of Function Here>
+/* @brief function to print the calculated statistics
+ * 
+ * this function takes in mininimum maximum mean and median of the inputted array
  *
- * <Add Extended Description Here>
- *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * @return void
  */
 
+void print_array(int arr[],int n)
 
+/* @brief function to print the given array
+ * 
+ * this function takes in the given array and number of elements and prints it
+ *
+ * @return void
+ */
+
+int find_min(int arr[],int n)
+
+/* @brief function to find the minimum
+ * 
+ * this function takes in the given array and number of elements and traverses through the array to find the minimum amoung the elements 
+ *
+ * @return integer min
+ */
+
+int find_max(int arr[],int n)
+
+/* @brief function to find the maximum
+ * 
+ * this function takes in the given array and number of elements and traverses through the array to find the miximum amoung the elements 
+ *
+ * @return integer max
+ */
+
+float find_median(int arr[],int n)
+
+/* @brief function to find the median
+ * 
+ * this function takes in the SORTED array and number of elements and finds the middle element by diving the number of elements by 2
+ *
+ * @return float median
+ */
+
+float find_mean(int arr[],int n)
+
+/* @brief function to find the mean
+ * 
+ * this function takes in the given array and number of elements and first finds the sum of all the elements and then divides by the total number of terms to get the mean
+ *
+ * @return float mean
+ */
+
+void sort_array(int arr[],int n)
+
+/* @brief function to sort the given array
+ * 
+ * this function takes in the given array and number of elements and sorts the given array from largest to smallest by getting the largest numbers to the initial positions
+ *
+ * @return void
+ */
 #endif /* __STATS_H__ */
